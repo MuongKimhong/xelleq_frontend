c7ca6e369709e93e4f2da323dfe41f40ba7be52a
Kim <muongkimhong086@gmail.com>
2025-08-13 15:22:29 +1000

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -7,7 +7,7 @@ import router from "@/router"
 // })
 
 const api = axios.create({
-  baseURL: 'https://plovkaapi.xelleq.com',
+  baseURL: 'https://aappii.xelleq.com',
   withCredentials: true,
 })
 
